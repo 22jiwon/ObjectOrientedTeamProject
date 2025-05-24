13350b0bcfb565f76a40ea62f5bad1c3bb222b73
2giyeon <1eegy@sookmyung.ac.kr>
2025-05-25 05:05:24 +0900

    마이페이지 & 판매목록 완성

---
+++ app/src/main/java/com/example/myapplication/SaleItem.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class SaleItem {
+    public String title;
+    public String price;
+    public String createdAt;
+    public String status;
+    public String imageUrl;
+    public boolean isSelected;
+
+    public SaleItem(String title, String price, String createdAt, String status, String imageUrl) {
+        this.title = title;
+        this.price = price;
+        this.createdAt = createdAt;
+        this.status = status;
+        this.imageUrl = imageUrl;
+        this.isSelected = false;
+    }
+}
